716d9881758a356b6e903f44fd4a18dc7a41f2e2
Oliver <oliversotof@gmail.com>
2022-01-13 15:37:55 -0600

    First Commit

---
+++ src/main/java/com/xtremedreamers/webuy/WebuyApplication.java
@@ -0,0 +1,13 @@
+package com.xtremedreamers.webuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebuyApplication.class, args);
+	}
+
+}
